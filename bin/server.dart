fefa52b4e71f1af1d407344a106ac4f54c3d6b21
Lukas Himsel <lukas@himsel.me>
2020-12-14 10:04:27 +0100

    fix server

--- bin/server.dart
+++ bin/server.dart
@@ -1,14 +1,14 @@
+import 'dart:async';
 import 'dart:io';
 import 'package:cloudrun_template/router.dart';
 
-main() async {
-  try {
-    await HttpServer.bind(
-      InternetAddress.loopbackIPv4,
-      int.tryParse(Platform.environment['PORT']) ?? 8080,
-    )
-      ..listen(requestHandler);
-  } catch (e) {
-    print(e);
-  }
+main() {
+  runZoned(() async {
+    try {
+      (await HttpServer.bind(InternetAddress.anyIPv4, 8080))
+        ..listen(requestHandler);
+    } catch (e) {
+      print('error $e');
+    }
+  });
 }
